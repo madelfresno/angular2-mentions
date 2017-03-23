8bdd11340df395cb91a2e7f528153f2318c8aba3
mafresno <mafresno@bebee.com>
2017-03-23 18:30:04 +0100

    spinner

--- src/mention/mention-list.component.ts
+++ src/mention/mention-list.component.ts
@@ -26,7 +26,7 @@ import { getCaretCoordinates } from './caret-coords';
     template: `
     <ul class="dropdown-menu scrollable-menu typeahead-mention" #list [hidden]="hidden">
         <li>
-          <img src="/three-dots.svg" alt="spinner" class="spinner" />
+          <img bbImage="/three-dots.svg" alt="spinner" class="spinner" />
         </li>
         <li *ngFor="let item of items; let i = index" [class.active]="activeIndex==i" (mousedown)="activeIndex=i;itemClick.emit();$event.preventDefault()">            
           <a class="dropdown-cnt-img-profile">
