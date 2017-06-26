c928070b3d0342d7bf1942784e85b4de81578b54
mafresno <mafresno@bebee.com>
2017-06-26 10:24:40 +0200

    hidden

--- mention/mention.directive.js
+++ mention/mention.directive.js
@@ -108,7 +108,7 @@ var MentionDirective = (function () {
                 !event.ctrlKey &&
                 pos > this.startPos) {
                 if (event.keyCode === KEY_BACKSPACE && pos > 0 && (pos - 1) != this.startPos) {
-                    this.searchList.hidden = true;
+                    this.searchList.hidden = this.stopSearch;
                     pos--;
                     if (this.timer)
                         clearTimeout(this.timer);
